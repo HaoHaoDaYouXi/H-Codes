0212db3a270254ab42e4e09f8fd4c45a3eb21999
Wangxt <2601183227@qq.com>
2024-09-06 00:46:42 +0800

    H

--- java/springboot-template/src/main/java/com/haohaodayouxi/demo/controller/HealthController.java
+++ java/springboot-template/src/main/java/com/haohaodayouxi/demo/controller/HealthController.java
@@ -1,10 +1,9 @@
 package com.haohaodayouxi.demo.controller;
 
+import com.haohaodayouxi.common.log.annotation.AutoLog;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
@@ -28,7 +27,13 @@ public class HealthController {
     @GetMapping("/h-open/check")
     public String healthCheck() {
         log.info("healthCheck");
-        return String.format(applicationName + " is health! %s", LocalDateTime.now().toString());
+        return String.format(applicationName + " is health! %s", LocalDateTime.now());
     }
 
+    @AutoLog(value = "测试日志切面", keepLogServiceName = "TmpKeepLogServiceImpl")
+    @PostMapping("/testLog")
+    public String testLog(@RequestBody String a) {
+        log.info("testLog a:{}", a);
+        return String.format(applicationName + " is health! %s", LocalDateTime.now());
+    }
 }
