a3441fbdb9e6b94532056f401d76e71ab5826c80
Wangxt <2601183227@qq.com>
2024-09-18 15:22:11 +0800

    H

--- html/v3-vite-template/src/main.ts
+++ html/v3-vite-template/src/main.ts
@@ -4,9 +4,13 @@ import App from '@/App.vue'
 
 // load
 import { loadDirectives } from "@/directives"
+import { loadSvg } from "@/icons"
 
 const app = createApp(App)
 
+// 加载自定义指令
 loadDirectives(app)
+// 加载全局 SVG
+loadSvg(app)
 
 app.mount("#app")
