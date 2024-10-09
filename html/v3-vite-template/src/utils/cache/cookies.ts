a3e21829ba7d559e6e98f198a78f610078e0f353
Wangxt <2601183227@qq.com>
2024-10-09 17:50:29 +0800

    H

--- html/v3-vite-template/src/utils/cache/cookies.ts
+++ html/v3-vite-template/src/utils/cache/cookies.ts
@@ -1,17 +1,6 @@
 /** 统一处理 Cookie */
+import CacheKey from "@/constants/cache-key"
 import Cookies from "js-cookie"
-
-const SYSTEM_NAME = import.meta.env.VITE_APP_TITLE
-
-/** 缓存数据时用到的 Key */
-class CacheKey {
-  static readonly TOKEN = `${SYSTEM_NAME}-token-key`
-  static readonly CONFIG_LAYOUT = `${SYSTEM_NAME}-config-layout-key`
-  static readonly SIDEBAR_STATUS = `${SYSTEM_NAME}-sidebar-status-key`
-  static readonly ACTIVE_THEME_NAME = `${SYSTEM_NAME}-active-theme-name-key`
-  static readonly VISITED_VIEWS = `${SYSTEM_NAME}-visited-views-key`
-  static readonly CACHED_VIEWS = `${SYSTEM_NAME}-cached-views-key`
-}
 
 export const getToken = () => {
   return Cookies.get(CacheKey.TOKEN)
