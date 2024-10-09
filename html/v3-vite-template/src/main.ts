a4b390a583cf233ff47b0f403ed1c9a0f6ec35d7
Wangxt <2601183227@qq.com>
2024-10-09 15:50:27 +0800

    H

--- html/v3-vite-template/src/main.ts
+++ html/v3-vite-template/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from '@/App.vue'
 import store from "@/store"
 import router from "@/router"
+import "@/router/permission"
 
 // load
 import { loadPlugins } from "@/plugins"
