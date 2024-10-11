c8001763a68d1e438da83bb6c7730dae9cdba4db
Wangxt <2601183227@qq.com>
2024-10-11 14:02:41 +0800

    H

--- html/v3-vite-template/src/router/index.ts
+++ html/v3-vite-template/src/router/index.ts
@@ -1,35 +1,38 @@
-import { type RouteRecordRaw, createRouter,
-    createWebHashHistory,createWebHistory } from 'vue-router'
+import { type RouteRecordRaw, createRouter } from "vue-router"
 import { history } from "./util"
 
 /**
  * 常驻路由
  */
 export const constantRoutes: RouteRecordRaw[] = [
-    {
-        path: '/',
-        component: () => import('@/views/index.vue'),
-        meta: {
-            hidden: true
-        }
-    },
-    {
-        path: '/login',
-        component: () => import('@/views/login/index.vue'),
-        meta: {
-            hidden: true
-        }
+  {
+    path: "/login",
+    component: () => import("@/views/login/index.vue"),
+    meta: {
+      hidden: true
     }
+  }
 ]
-
-export const dynamicRoutes: RouteRecordRaw[] = [
-
-]
-
 
 const router = createRouter({
-    history,
-    routes: constantRoutes
+  history,
+  routes: constantRoutes
 })
+
+/** 重置路由 */
+export function resetRouter() {
+  // 路由必须带有 Name 属性，否则会不能重置干净
+  try {
+    router.getRoutes().forEach((route) => {
+      const { name } = route
+      if (name) {
+        router.hasRoute(name) && router.removeRoute(name)
+      }
+    })
+  } catch {
+    // 强制刷新浏览器也行，只是交互体验不是很好
+    window.location.reload()
+  }
+}
 
 export default router
