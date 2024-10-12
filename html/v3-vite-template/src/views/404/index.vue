20e16c235c952b57e1a91925f5a086bd9fa61e59
Wangxt <2601183227@qq.com>
2024-10-12 16:42:34 +0800

    H

--- html/v3-vite-template/src/views/404/index.vue
+++ html/v3-vite-template/src/views/404/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="content">
+  <div class="content404">
     <p class="title">页面不存在</p>
     <p class="explain">请确认您输入的跳转地址是否正确，或者单击下方的按钮返回主页。</p>
     <router-link class="el-icon-back" tag="a" to="/" replace>
@@ -9,48 +9,48 @@
 </template>
 
 <style lang="scss" scoped>
-.content {
+.content404 {
   position: absolute;
   left: 47%;
   top: 38%;
   transform: translate(-50%, -50%);
   z-index: 1;
   text-align: left;
-}
 
-.title {
-  line-height: 1.391304;
-  font-size: 46px;
-  font-family:
-    PingFang SC-Bold,
-    PingFang SC;
-  color: #333333;
-}
+  .title {
+    line-height: 1.391304;
+    font-size: 46px;
+    font-family:
+      PingFang SC-Bold,
+      PingFang SC;
+    color: #333333;
+  }
 
-.explain {
-  margin-top: 16px;
-  line-height: 1.285714;
-  font-size: 32px;
-  font-family:
-    PingFang SC-Bold,
-    PingFang SC;
-  color: #666666;
-}
+  .explain {
+    margin-top: 16px;
+    line-height: 1.285714;
+    font-size: 32px;
+    font-family:
+      PingFang SC-Bold,
+      PingFang SC;
+    color: #666666;
+  }
 
-.el-icon-back {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 160px;
-  height: 60px;
-  margin-top: 32px;
-  margin-left: 90%;
-  color: #fff;
-  background: #2975f9;
-  font-size: 24px;
+  .el-icon-back {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 160px;
+    height: 60px;
+    margin-top: 32px;
+    margin-left: 90%;
+    color: #fff;
+    background: #2975f9;
+    font-size: 24px;
 
-  span {
-    margin-left: 6px;
+    span {
+      margin-left: 6px;
+    }
   }
 }
 </style>
