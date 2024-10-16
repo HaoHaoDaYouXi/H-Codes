a6470d3be8ba59a6cbe2ad2e80b5941eca76c2d0
Wangxt <2601183227@qq.com>
2024-10-16 20:51:33 +0800

    H

---
+++ html/v3-vite-template/src/store/modules/permission.ts
@@ -0,0 +1,37 @@
+﻿import { ref } from "vue"
+import store from "@/store"
+import { defineStore } from "pinia"
+import { type RouteRecordRaw } from "vue-router"
+import { constantRoutes } from "@/router"
+
+export const usePermissionStore = defineStore("permission", () => {
+  /** 可访问的路由 */
+  const routes = ref<RouteRecordRaw[]>([])
+  /** 是否已经添加路由 */
+  const booAddRoutes = ref<boolean>()
+  /** 添加路由 */
+  const addRoutes = ref<RouteRecordRaw[]>([])
+
+  /** 获取路由 */
+  const getRouterByUser = async () => {
+    // await getRouterByUser() 调用接口获取路由信息
+    addRoutes.value = [
+      {
+        path: "/",
+        component: () => import("@/views/index.vue"),
+        meta: {
+          hidden: true
+        }
+      }
+    ]
+    booAddRoutes.value = true
+    return addRoutes.value
+  }
+
+  return { routes, booAddRoutes, addRoutes, getRouterByUser }
+})
+
+/** 在 setup 外使用 */
+export function usePermissionStoreHook() {
+  return usePermissionStore(store)
+}
