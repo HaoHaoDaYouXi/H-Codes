a4130c3ff6cff31dd61c1e18d95b7b8b78dd1ef2
Wangxt <2601183227@qq.com>
2024-10-19 13:02:39 +0800

    H

--- html/v3-vite-template/src/store/modules/permission.ts
+++ html/v3-vite-template/src/store/modules/permission.ts
@@ -8,29 +8,46 @@ import { PermissionData } from "@/api/permission/types/permission"
 
 export const usePermissionStore = defineStore("permission", () => {
   /** 可访问的路由 */
-  const routes = ref<RouteRecordRaw[]>([])
+  const routes = ref<RouteRecordRaw[]>(constantRoutes)
   /** 是否已经添加路由 */
   const booAddRoutes = ref<boolean>()
   /** 添加路由 */
-  const addRoutes = ref<RouteRecordRaw[]>([])
+  const addRouter = ref<RouteRecordRaw[]>([])
 
   /** 获取路由 */
   const getRouterByUser = async () => {
-    // await getRouterByUser() 调用接口获取路由信息
-    addRoutes.value = [
-      {
-        path: "/",
-        component: () => import("@/views/index.vue"),
-        meta: {
-          hidden: true
-        }
+    // const { data } = await getRouterByUserApi() // 调用接口获取路由信息
+    const { data } = {
+      data: {
+        routerDetails: [
+          {
+            id: "1",
+            parentId: "0",
+            resType: 2,
+            path: "/",
+            component: "Index",
+            disabled: 0,
+            meta: {
+              title: "首页",
+              hidden: false
+            }
+          }
+        ]
       }
-    ]
-    booAddRoutes.value = true
-    return addRoutes.value
+    }
+    // 格式化路由对象
+    data.routerDetails.forEach((route) => {
+      const tmp: RouteRecordRaw = {
+        ...route,
+        component: componentsMap[route.component],
+        name: route.component === "Layout" ? route.path.replace(/^\//, "") : route.component
+      }
+      addRouter.value.push(tmp)
+    })
+    routes.value.concat(addRouter.value)
   }
 
-  return { routes, booAddRoutes, addRoutes, getRouterByUser }
+  return { routes, booAddRoutes, addRouter, getRouterByUser }
 })
 
 /** 在 setup 外使用 */
