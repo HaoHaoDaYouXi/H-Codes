a8b3bdbb7595b5c25ff21f993c7de463cb1c249b
Wangxt <2601183227@qq.com>
2024-10-20 23:17:08 +0800

    H

--- html/v3-vite-template/src/store/modules/permission.ts
+++ html/v3-vite-template/src/store/modules/permission.ts
@@ -4,7 +4,7 @@ import { defineStore } from "pinia"
 import { type RouteRecordRaw } from "vue-router"
 import { componentsMap, constantRoutes } from "@/router"
 import { getRouterByUserApi } from "@/api/permission"
-import { PermissionData } from "@/api/permission/types/permission"
+import { PermissionRes, PermissionData} from "@/api/permission/types/permission"
 
 export const usePermissionStore = defineStore("permission", () => {
   /** 可访问的路由 */
