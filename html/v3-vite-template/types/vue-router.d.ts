be5e4220cc90aeae4f745ff218167d27209d5d20
Wangxt <2601183227@qq.com>
2024-11-15 00:47:09 +0800

    H

--- html/v3-vite-template/types/vue-router.d.ts
+++ html/v3-vite-template/types/vue-router.d.ts
@@ -13,6 +13,10 @@ declare module "vue-router" {
      */
     icon?: string
     /**
+     * 设置该路由的图标 elIcon
+     */
+    elIcon?: string
+    /**
      * 默认 false，设置 true 的时候该路由不会在侧边栏出现
      */
     hidden?: boolean
