83f4c237bfc8cb0c2742ec22d3517dcd138a203e
Wangxt <2601183227@qq.com>
2024-11-15 00:46:56 +0800

    H

--- html/v3-vite-template/src/icons/index.ts
+++ html/v3-vite-template/src/icons/index.ts
@@ -1,8 +1,11 @@
 import { type App } from "vue"
 import "virtual:svg-icons-register"
-
+import * as ElementPlusIconsVue from "@element-plus/icons-vue"
 import SvgIcon from "@/components/SvgIcon/index.vue"
 
-export function loadSvg(app: App) {
+export function loadIcons(app: App) {
+  for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component("el-icon-" + key, component)
+  }
   app.component("SvgIcon", SvgIcon)
 }
