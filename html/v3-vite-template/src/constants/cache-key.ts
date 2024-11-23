70c1ae8f0d2b8d3ef45550c5c0a695828d0d2f52
Wangxt <2601183227@qq.com>
2024-11-24 00:00:37 +0800

    H

--- html/v3-vite-template/src/constants/cache-key.ts
+++ html/v3-vite-template/src/constants/cache-key.ts
@@ -9,6 +9,7 @@ class CacheKey {
   static readonly ACTIVE_THEME_NAME = `${SYSTEM_NAME}-active-theme-name-key`
   static readonly VISITED_VIEWS = `${SYSTEM_NAME}-visited-views-key`
   static readonly CACHED_VIEWS = `${SYSTEM_NAME}-cached-views-key`
+  static readonly LOCALE = `${SYSTEM_NAME}-locale-key`
 }
 
 export default CacheKey
