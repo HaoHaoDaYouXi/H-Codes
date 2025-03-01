f84ac912f550fdb04cdca26e855b0eeaf3566935
Wangxt <2601183227@qq.com>
2025-03-01 21:15:04 +0800

    H

--- java/security/springboot-auth/src/main/java/com/haohaodayouxi/auth/intercepter/UserCheckInterceptor.java
+++ java/security/springboot-auth/src/main/java/com/haohaodayouxi/auth/intercepter/UserCheckInterceptor.java
@@ -7,6 +7,7 @@ import com.haohaodayouxi.common.core.constants.InterceptorCode;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -40,19 +41,28 @@ public class UserCheckInterceptor implements HandlerInterceptor {
         // 根据认证状态，获取上一步已取出的用户信息，判断用户是否可访问
         // 2&4=0   4&4=4   6&4=4
         if ((authStatus & InterceptorCode.TOKEN) != 0) {
-            LoginCacheBO bo = (LoginCacheBO) CurrentUserContextHolder.get();
             // todo 用户不可访问时直接返回错误信息 此处根据不能访问的用户、角色、身份等等缓存进行判断，
             //  尽量不要涉及到数据库查询，统一通过缓存进行判断，缓存内提前记录不能访问的一些限制标志，用作此处判断
-//            if(用户不可访问) {
-//                log.error("用户不可访问");
-//                InterceptorErrorResponse.responseErrorByCode(response, InterceptorCode.USER);
-//                return false;
-//            }
+            LoginCacheBO bo = (LoginCacheBO) CurrentUserContextHolder.get();
+            // 查询用户信息是否还可以使用
+            // ...todo查询用户是否有不可使用标识
+            if (ObjectUtils.isEmpty(bo)) {
+                // 用户信息不可以使用情况，并且接口为非公开接口
+                if (authStatus.equals(InterceptorCode.TOKEN)) {
+                    log.error("用户不可以使用，无法访问");
+                    InterceptorErrorResponse.responseErrorByCode(response, InterceptorCode.USER);
+                    return false;
+                }
+                // 6
+            } else {
+                // 4|8=12   6|8=14
+                CurrentParam.put(CurrentParam.AUTH_STATUS_KEY, authStatus | InterceptorCode.USER);
+            }
         }
-        CurrentParam.put(CurrentParam.AUTH_STATUS_KEY, authStatus | InterceptorCode.USER);
-        // 1010 authStatus=10  公开接口，token无效，用户可访问
-        // 1100 authStatus=12  非公开接口，token有效，用户可访问
-        // 1110 authStatus=14  公开接口，token有效，用户可访问
+        // 10 authStatus=2   公开接口，token无效，无用户信息
+        // 110 authStatus=6  公开接口，token有效，用户信息不可用
+        // 1100 authStatus=12  非公开接口，token有效，用户信息可用
+        // 1110 authStatus=14  公开接口，token有效，用户信息可用
         log.debug("UserCheckInterceptor status={}", CurrentParam.get(CurrentParam.AUTH_STATUS_KEY));
         return true;
     }
