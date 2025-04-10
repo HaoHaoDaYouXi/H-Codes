3bb4e6332412acdf9bb06800e25d22f4ef09819a
Wangxt <2601183227@qq.com>
2025-04-10 22:23:05 +0800

    H

---
+++ html/electron-app-demo/vue.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: './',
+  pluginOptions: {
+    electronBuilder: {
+      chainWebpackMainProcess: (config) => {
+        config.output.filename('background.js');
+      }
+    }
+  }
+})
