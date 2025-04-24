462d797f7bcdc8665262535a1aef151ebe312003
Wangxt <2601183227@qq.com>
2025-04-24 22:34:42 +0800

    H

--- html/v3-vite-template/src/router/permission.ts
+++ html/v3-vite-template/src/router/permission.ts
@@ -13,13 +13,18 @@ router.beforeEach(async (to, _from, next) => {
   NProgress.start()
   const token = getToken()
   // console.log("token: "+token)
-  // console.log(router.getRoutes())
+  console.log(to.path)
   // 如果没有登陆
   if (!token) {
     // 如果在免登录的白名单中，则直接进入
     if (isWhiteList(to)) return next()
     // 其他没有访问权限的页面将被重定向到登录页面
     return next("/login")
+  }
+
+  // 如果已经登录，并准备进入 Login 页面，则重定向到主页
+  if (to.path === "/login") {
+    return next({ path: "/" })
   }
 
   const userStore = useUserStoreHook()
